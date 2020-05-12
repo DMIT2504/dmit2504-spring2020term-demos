7f12319cb6ee073b50055b8dd52cdeaff6c402ef
Sam Wu <sam.c.wu@gmail.com>
2020-05-11 18:59:23 -0600

    Added Lesson 2.1 and 2.2

---
+++ JitterApp/app/src/main/java/ca/nait/dmit2504/jitterapp/MainActivity.java
@@ -0,0 +1,78 @@
+package ca.nait.dmit2504.jitterapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();   // simpleName = "MainActivity"
+
+    private TextView mJitterText;
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_list_jitters:
+                Intent listJittersIntent = new Intent(this, MainActivity.class);
+                startActivity(listJittersIntent);
+                return true;
+            case R.id.menu_item_post_jitter:
+                Intent postJitterIntent = new Intent(this, SendJitterActivity.class);
+                startActivity(postJitterIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // find the textview in the layout
+        mJitterText = findViewById(R.id.jitters_text);
+
+        // Generate an implementation of the Retrofit interface
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://www.youcode.ca")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+        YoucodeService youcodeService = retrofit.create(YoucodeService.class);
+
+        // Call a method in your service
+        Call<String> getCall = youcodeService.listJSONServlet();
+        getCall.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(final Call<String> call, final Response<String> response) {
+                Log.i(TAG,"success getting data");
+                mJitterText.setText(response.body());
+            }
+
+            @Override
+            public void onFailure(final Call<String> call, final Throwable t) {
+                Log.e(TAG, "failure to get data");
+            }
+        });
+
+    }
+}
