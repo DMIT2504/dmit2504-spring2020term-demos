ae37510fc8dbfad789f1f0b2b3ca1258f13fae95
Sam Wu <sam.c.wu@gmail.com>
2020-06-26 18:52:56 -0600

    add products table

--- NorthwindTradersApp/app/src/main/java/ca/nait/dmit2504/northwindtradersapp/NorthwindTradersDatabase.java
+++ NorthwindTradersApp/app/src/main/java/ca/nait/dmit2504/northwindtradersapp/NorthwindTradersDatabase.java
@@ -13,12 +13,19 @@ import java.util.ArrayList;
 public class NorthwindTradersDatabase extends SQLiteOpenHelper {
 
     // Step 2: Define data fields for database name, table name, and column names
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 3;
     private static final String DATABASE_NAME = "NorthwindTraders.db";
     public static final String TABLE_CATEGORY = "category";
     public static final String TABLE_CATEGORY_COLUMN_ID = BaseColumns._ID;
     public static final String TABLE_CATEGORY_COLUMN_NAME = "categoryName";
     public static final String TABLE_CATEGORY_COLUMN_DESCRIPTION = "description";
+
+    public static final String TABLE_PRODUCT = "product";
+    public static final String TABLE_PRODUCT_COLUMN_ID = BaseColumns._ID;   // "_id"
+    public static final String TABLE_PRODUCT_COLUMN_PRODUCT_NAME = "productName";
+    public static final String TABLE_PRODUCT_COLUMN_UNIT_PRICE = "unitPrice";
+    public static final String TABLE_PRODUCT_COLUMN_CATEGORYID = "categoryID";
+
 
     // Step 3: Define a constructor with a Context parameter
     public NorthwindTradersDatabase(Context context) {
@@ -29,10 +36,25 @@ public class NorthwindTradersDatabase extends SQLiteOpenHelper {
     @Override
     public void onCreate(final SQLiteDatabase db) {
         db.execSQL("CREATE TABLE category (_id integer primary key, categoryName TEXT, description TEXT);");
+        db.execSQL("CREATE TABLE product (_id integer primary key, productName TEXT, unitPrice TEXT, categoryID integer REFERENCES category(_id))");
+
+        // Create sample data
+        db.execSQL("INSERT INTO category(categoryName) VALUES('Seafood');");
+        db.execSQL("INSERT INTO category(categoryName) VALUES('Meat');");
+        db.execSQL("INSERT INTO category(categoryName) VALUES('Soft Drinks');");
+
+        db.execSQL("INSERT INTO product(productName,unitPrice,categoryID) VALUES('Lobster','100', 1);");
+        db.execSQL("INSERT INTO product(productName,unitPrice,categoryID) VALUES('Crab','200', 1);");
+        db.execSQL("INSERT INTO product(productName,unitPrice,categoryID) VALUES('Fish','50', 1);");
+        db.execSQL("INSERT INTO product(productName,unitPrice,categoryID) VALUES('Coca-Cola','5', 3);");
+        db.execSQL("INSERT INTO product(productName,unitPrice,categoryID) VALUES('Pepsi','4', 1);");
+
+
     }
 
     @Override
     public void onUpgrade(final SQLiteDatabase db, final int oldVersion, final int newVersion) {
+        db.execSQL("DROP TABLE product;");
         db.execSQL("DROP TABLE category;");
         onCreate(db);
     }
@@ -49,6 +71,18 @@ public class NorthwindTradersDatabase extends SQLiteOpenHelper {
 
         // Call the insert method
         return db.insert(TABLE_CATEGORY, null, values);
+    }
+
+    // Return a cursor of product for a given category
+    public Cursor findProductByCategory(int categoryID) {
+        // Get a readable database
+        SQLiteDatabase db = getReadableDatabase();
+        // Construct SQL statement to return product for a given category
+        String queryStatement = "SELECT _id, productName, unitPrice FROM product WHERE categoryID = ?";
+        String[] selectArgs = {String.valueOf(categoryID)};
+
+        // Execute and return the query results
+        return db.rawQuery(queryStatement, selectArgs);
     }
 
     // Return a cursor with all categories
